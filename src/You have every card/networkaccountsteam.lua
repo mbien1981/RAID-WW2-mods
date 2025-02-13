c77df78a0f8367ac2ede99fd84fdf1f59a4fe3f6
mbien1981 <shieldhater@gmail.com>
2025-02-13 19:27:00 +0100

    Add updated version of 'You have every card' by Punished Bernadetta

---
+++ src/You have every card/networkaccountsteam.lua
@@ -0,0 +1,37 @@
+function NetworkAccountSTEAM:_clbk_inventory_load(error, list)
+	self._inventory_is_loading = nil
+	_G.ToolBox:save_table_to_file2(list, 1000, "data/ass2.lua")
+
+	list = {}
+	local cards = tweak_data.challenge_cards:get_all_cards_indexed()
+
+	log("HELL")
+	_G.ToolBox:save_table_to_file2(cards, 1000, "data/ass.lua")
+	for i = 1, #cards, 1 do
+		table.insert(list, {
+			bonus = false, --Always false
+			def_id = cards[i]["def_id"], --Card ID number
+			amount = table.random({ 1, 69, 420, 999 }), --It doesn't matter what number this is - will count as 1 by the game anyway
+			category = "challenge_card",
+			quality = "", --Always nil
+			entry = cards[i]["key_name"], --Card name
+			instance_id = 0, --A unique ID for each instance of the card.
+		})
+	end
+
+	local filtered_cards = self:_verify_filter_cards(list)
+	local filtered_crafts = self:_verify_filter_crafts(list)
+
+	managers.system_event_listener:call_listeners(
+		CoreSystemEventListenerManager.SystemEventListenerManager.EVENT_STEAM_INVENTORY_LOADED,
+		{
+			error = error,
+			cards = filtered_cards,
+			crafts = filtered_crafts,
+		}
+	)
+end
+
+function NetworkAccountSTEAM:inventory_remove(instance_id)
+	--Nothing!
+end
